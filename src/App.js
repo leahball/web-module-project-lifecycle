3a284da5c36514db67f0feee30d1a21d9d350c6b
iamleahb <leah.ball@gmail.com>
2022-01-11 19:16:16 -0600

    followers div mocked in, ready for components

--- src/App.js
+++ src/App.js
@@ -9,13 +9,46 @@ class App extends React.Component {
           <input placeholder="Github Handle" />
           <button>Search</button>
         </form>
-        <img src="https://avatars.githubusercontent.com/u/91203600?v=4" />
-        <a target="_blank" href="https://github.com/leahball">
-          <h3>Leah Ball</h3>
-        </a>
-        <p>Username: leahball</p>
-        <p>Total Repos: 34 </p>
-        <p>Total Followers: 17</p>
+
+        <div id="usercard">
+          <img src="https://avatars.githubusercontent.com/u/91203600?v=4" />
+          <a target="_blank" href="https://github.com/leahball">
+            <h3>Leah Ball</h3>
+          </a>
+          <p>Username: leahball</p>
+          <p>Total Repos: 34 </p>
+          <p>Total Followers: 17</p>
+        </div>
+
+        <div id="followers">
+          <div className="follower">
+            <img
+              width="200px"
+              src="https://avatars.githubusercontent.com/u/64448617?v=4"
+            />
+            <a target="_blank" href="https://github.com/nyamekyeannor">
+              <p>nyamekyeannor</p>
+            </a>
+          </div>
+          <div className="follower">
+            <img
+              width="200px"
+              src="https://avatars.githubusercontent.com/u/64448617?v=4"
+            />
+            <a target="_blank" href="https://github.com/nyamekyeannor">
+              <p>nyamekyeannor</p>
+            </a>
+          </div>
+          <div className="follower">
+            <img
+              width="200px"
+              src="https://avatars.githubusercontent.com/u/64448617?v=4"
+            />
+            <a target="_blank" href="https://github.com/nyamekyeannor">
+              <p>nyamekyeannor</p>
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
