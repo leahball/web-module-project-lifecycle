ceeea444b78a4682042ae80679b00860f5fd4aae
iamleahb <leah.ball@gmail.com>
2022-01-11 18:55:27 -0600

    mocked up userinfowith hardcoded data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
   render() {
-    return(<div>
-      Github Card
-    </div>);
+    return (
+      <div>
+        <h1>Github Info</h1>
+        <form>
+          <input placeholder="Github Handle" />
+          <button>Search</button>
+        </form>
+        <img src="https://avatars.githubusercontent.com/u/91203600?v=4" />
+        <a target="_blank" href="https://github.com/leahball">
+          <h3>Leah Ball</h3>
+        </a>
+        <p>Username: leahball</p>
+        <p>Total Repos: 34 </p>
+        <p>Total Followers: 17</p>
+      </div>
+    );
   }
 }
 
